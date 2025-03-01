3d6272f304ab3bd0da7e1e8e5c34fc114bf1ea8e
Kasereka Kighunda <Kaserekakighunda9@gmail.com>
2025-03-01 17:52:42 +0200

    creation of function _interaciveWidget and call in textField

--- lib/car_selector_page.dart
+++ lib/car_selector_page.dart
@@ -9,6 +9,31 @@ class CarSelectorPage extends StatefulWidget {
 }
 
 class _CarSelecorPageState extends State<CarSelectorPage> {
+  String _firstName = "";
+
+  Padding _interactiveWidget(
+      {required List<Widget> children, bool isRow = false}) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        children: [
+          (isRow)
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: children,
+                )
+              : Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: children,
+                ),
+          Divider()
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +46,26 @@ class _CarSelecorPageState extends State<CarSelectorPage> {
           )
         ],
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Bienvenue: $_firstName"),
+            _interactiveWidget(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: "Entrez votre nom"),
+                  onSubmitted: (newValue) {
+                    setState(() {
+                      _firstName = newValue;
+                    });
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
